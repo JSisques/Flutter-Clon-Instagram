d0ec4099943fb77bdca098019175967d22daaf36
Javi Plaza <javierplazasisques@gmail.com>
2021-02-07 12:04:22 +0100

    App Bar updated

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,5 @@
 import 'package:clon_instagram/screens/splashScreen.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget{
--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
 class HomeScreen extends StatelessWidget{
 
